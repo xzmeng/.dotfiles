3640181d8867c5a8864dc20fde176e07b3a6f84e
Meng Xiangzhuo <aumo@foxmail.com>
2021-03-20 04:40:41 +0800

    update

--- install_macos.sh
+++ install_macos.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # macOS
 
+# first install: 
+# - homebrew
+# - tmux
+# - ohmyzsh
+
+# uncomment if using a new system
+
+# cp .zshrc ~/.zshrc
+# cp .gitconfig ~/.gitconfig
+
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 DIRNAME=$PWD
 
 ##
@@ -15,6 +27,8 @@ ln -s $HOME/.tmux/.tmux.conf.local
 ln -s $DIRNAME/.vim
 ln -s $HOME/.vim/.vimrc
 ln -s $HOME/.vim/.vimrc.local
+
+## 
 
 ####
 cd -
