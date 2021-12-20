abde4bf606c24a01dfa3340d628af5e8192e1cfa
Meng Xiangzhuo <aumo@foxmail.com>
2021-12-20 11:45:38 +0800

    mac

--- install_macos.sh
+++ install_macos.sh
@@ -19,14 +19,13 @@ DIRNAME=$PWD
 cd
 
 # tmux conf
-ln -s $DIRNAME/.tmux
-ln -s $HOME/.tmux/.tmux.conf
-ln -s $HOME/.tmux/.tmux.conf.local
+ln -s $DIRNAME/.tmux/.tmux.conf
+ln -s $DIRNAME/.tmux/.tmux.conf.local
 
 # vim conf
 ln -s $DIRNAME/.vim
-ln -s $HOME/.vim/.vimrc
-ln -s $HOME/.vim/.vimrc.local
+ln -s $DIRNAME/.vim/.vimrc
+ln -s $DIRNAME/.vim/.vimrc.local
 
 ## 
 
