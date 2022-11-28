ce2f60273a31dd27804dfffca3c22787f47e82f4
xzmeng <aumo@foxmail.com>
2022-11-29 00:41:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 # ohmyzsh theme
-sed -i 's/ZSH_THEME=.*/ZSH_THEME="gallifrey"/' ~/.zshrc
+sed -i 's/ZSH_THEME=.*/ZSH_THEME="blinks"/' ~/.zshrc
 # ohmyzsh plugins
 if [ ! -d "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
