b4c6d6bef22d842abb3e7d49171a94856d267276
xzmeng <aumo@foxmail.com>
2022-12-08 12:05:44 +0800

    update gitpod

--- install.sh
+++ install.sh
@@ -2,23 +2,4 @@
 
 # for gitpod
 
-# timezone
-sudo echo /Asia/Shanghai > /etc/timezone
-sudo rm /etc/localtime
-sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
-# ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-# ohmyzsh theme
-sed -i 's/ZSH_THEME=.*/ZSH_THEME="blinks"/' ~/.zshrc
-# ohmyzsh plugins
-if [ ! -d "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
-sed -i 's/^plugins=(.*/plugins=(docker docker-compose zsh-autosuggestions z)/' ~/.zshrc
-
-# git
-cp ~/.dotfiles/.gitconfig ~/.gitconfig
-
-# pyenv
-echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+/bin/bash ./install_gitpod.sh
---
+++ install_gitpod.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# initialize
+source <(bash -lic 'declare -px')
+
+# timezone
+sudo echo /Asia/Shanghai > /etc/timezone
+sudo rm /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+# ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# ohmyzsh plugins
+if [ ! -d "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+# .zshrc
+cp .gitpod.zshrc ~/.zshrc
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+# git
+cp ~/.dotfiles/.gitconfig ~/.gitconfig
